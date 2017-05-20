70050e7f6b730256d8fea05d89139a3c55a8c7e4
Josh Boyan <joshboyan@yahoo.com>
2017-05-19 18:52:55 -0700

    GA sw tracking

--- src/index.js
+++ src/index.js
@@ -5,21 +5,31 @@ import { App } from './components/App'
 
 window.React = React;
 
+render(
+	<App />,
+	document.getElementById('reactTarget')
+)
+
 // Check for browser support of service worker
 if ('serviceWorker' in navigator) {
  navigator.serviceWorker.register('../service-worker.js')// change to ./service-worker.js for production
  .then(function(registration) {
    // Successful registration
    console.log('Hooray. Registration successful, scope is:', registration.scope);
+   ga('send', 'event', 'Service-worker', 'install', 'Successfully installed sw');
  }).catch(function(err) {
    // Failed registration, service worker won’t be installed
    console.log('Whoops. Service worker registration failed, error:', err);
+    ga('send', 'event', 'Service-worker', 'install', 'Failed installing sw');
+
  });
 }
 
-render(
-	<App />,
-	document.getElementById('reactTarget')
-)
-
+document.addEventListener('scroll', function (event) {
+    if (document.body.scrollHeight ==
+        document.body.scrollTop +        
+        window.innerHeight) {
+        ga('send', 'event', 'App', 'scroll', 'Scrolled to bottom');
+    }
+});
 
